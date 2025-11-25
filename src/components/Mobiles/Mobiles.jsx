658b94aac8fd5000bea02bf31ed77ee6fac305eb
nothing36 <prodipsarker953@gmail.com>
2025-11-25 22:39:48 +0600

    route and sub route added

---
+++ src/components/Mobiles/Mobiles.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mobiles = () => {
+    return (
+        <div>
+            <h3>This is mobile section</h3>
+        </div>
+    );
+};
+
+export default Mobiles;
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,29 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from './components/Root/Root.jsx';
+import Mobiles from './components/Mobiles/Mobiles.jsx';
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Laptops from './components/Laptops/Laptops.jsx';
+
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello from react router!!</div>,
+  path:"/",
+  Component:Root,
+  children:[
+    {index: true, Component:Home},
+    {path:'mobiles',Component:Mobiles},
+    {path:'laptops',Component:Laptops},
+    {path:'about', Component:About}
+
+  ]
   },
+
 ]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={router}></RouterProvider>
+  <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
