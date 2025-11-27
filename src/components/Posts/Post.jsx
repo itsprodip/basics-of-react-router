17a2a974f893817736554c55988efebffc91d9ed
nothing36 <prodipsarker953@gmail.com>
2025-11-27 13:43:36 +0600

    route using navigate

--- src/components/Posts/Post.jsx
+++ src/components/Posts/Post.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
 const Post = ({post}) => {
     const {title,id}=post;
+    const navigate=useNavigate();
+    const handleNavigate=()=>{
+        navigate(`/posts/${id}`);
+    }
     return (
         <div>
             <h2>{id}.{title}</h2>
             <Link to={`/posts/${id}`}><button>View Post</button></Link>
+            <button onClick={handleNavigate}>Details of {id}</button>
         </div>
     );
 };
--- src/components/Posts/PostDetails.jsx
+++ src/components/Posts/PostDetails.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const PostDetails = () => {
     const post=useLoaderData();
     // console.log(post);
-
+    const navigate=useNavigate();
     return (
         <div>
             <h1>Post details here</h1>
             <h2>Title:{post.title}</h2>
             <p>{post.body}</p>
+            <button onClick={()=>navigate(`/posts`)}>Go back</button>
         </div>
     );
 };
